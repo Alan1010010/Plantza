da49ac0887a812e1bb7910c3f3551d40b137f156
Alan Quinn <alancquinn2@gmail.com>
2019-08-22 14:23:42 +0100

    add update and destroy to booking system. add dynamic show page.

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,8 +1,7 @@
 class Plant < ApplicationRecord
   belongs_to :user
   mount_uploader :photo, PhotoUploader
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
   validates :name, :price, :address, presence: true
-
 end
