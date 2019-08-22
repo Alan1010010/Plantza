a33c34839b5c901eff036ea22098b59e602d20e6
Stanislas Bussiere <stanislas.bussiere@gmail.com>
2019-08-22 13:58:32 +0100

    better looking map (corrected)

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -7,7 +7,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/stanbssr/cjzmoweek00so1dp6wo6lw8mj'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
